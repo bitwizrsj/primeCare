22425aa676a7743a7a0a174b82ad200507804060
bitwizrsj <158316834+bitwizrsj@users.noreply.github.com>
2025-05-19 23:51:28 +0530

    Create All Pages and Setup Routing

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from './Hero'
 // import MedicalInsight from './MedicalInsight'
-import MedicalTeamSection from './MedicalTeamSection'
+// import MedicalTeamSection from './MedicalTeamSection'
 import ServicesSection from './ServicesSection'
 import CareerCTASection from './CareerCTASection'
 
